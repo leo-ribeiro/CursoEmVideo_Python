4e3160ff233554dc536e2c0bb59fc1f500f6d837
leopempem <leopemribeiro@gmail.com>
2022-07-27 17:51:08 -0300

    Primeiro Commit

---
+++ CursoEmVideo/PythonExercicios/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu nome:')
+print('Prazer', nome, 'Seja bem-vindo!')
+print('É um prazer te conhecer, {}!' .format(nome))
---
+++ scripts-python/desafio3.py
@@ -0,0 +1,3 @@
+primeiro = input ('Primeiro Número = ')
+segundo = input ('Segundo Número = ')
+print('A soma é ', primeiro + segundo)
